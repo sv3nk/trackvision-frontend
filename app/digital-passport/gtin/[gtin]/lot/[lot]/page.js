e47acef42e912ce3ac465db7d5868cb597a60fec
= <=>
2025-07-10 15:45:35 +0200

    trackvision queries

--- app/digital-passport/gtin/[gtin]/lot/[lot]/page.js
+++ app/digital-passport/gtin/[gtin]/lot/[lot]/page.js
@@ -1,5 +1,21 @@
+import { getCalculationOuput, getIngredientsList, getMaterialOriginList, getComponentDetailsList, getAdditiveList, getRecyclabilityList, getRecommendedUseList } from "@/lib/data";
+
 
 export default async function Page({ params }) {
-    const { gtin, lot } = await params
-    return <div>GTIN: {gtin} LOT: {lot}</div>
+    const { gtin, lot } = await params;
+
+    const calculationData = await getCalculationOuput(gtin, lot);
+    //const ingredientList = await getIngredientsList(gtin, lot);
+    //const materialOriginList = await getMaterialOriginList(gtin, lot);
+    //const componentDetailsList = await getComponentDetailsList(gtin, lot);
+    //const additiveList = await getAdditiveList(gtin, lot);
+    //const recyclabilityList = await getRecyclabilityList(gtin, lot);
+    //const recommendedUseList = await getRecommendedUseList(gtin, lot);
+
+
+    return (
+        <div>
+            
+        </div>
+    )
 }
